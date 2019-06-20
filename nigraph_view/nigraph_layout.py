1cbc9e280a09b974ef58bc1afb9d7ca16714bba0
ShacharGal <37271251+ShacharGal@users.noreply.github.com>
2019-06-20 11:11:16 +0300

    conn_mat works

--- nigraph_view/nigraph_layout.py
+++ nigraph_view/nigraph_layout.py
@@ -16,7 +16,7 @@ total_width = 1200
 data = ng.Scan()
 
 # Header
-header = PreText(text="Welcome to NyGraph_View", style={'font-size': '200%', 'color': 'blue'})
+header = PreText(text="Welcome to NiGraph_View", style={'font-size': '200%', 'color': 'blue'})
 warning = PreText(text="Warning Window: No Warning for Now!")
 
 # Choose map
@@ -39,49 +39,36 @@ accept_atlas_and_meta_path = Button(label="Accept Atlas and Metadata Input", wid
 accept_atlas_and_meta_path.on_click(accept_atlas_and_meta_button)
 
 # Connectivity
-def name_array(names):
-    x_labels = [list(names) for i in range(len(names))]
-    y_labels = x_labels[::-1]
+def label_array(orig_list):
+    x_labels = [[list(orig_list) for i in range(len(orig_list))]]
+    revered_labels = orig_list[::-1]
+    y_labels = [[[revered_labels[i]]*len(revered_labels) for i in range(len(revered_labels))]]
     return x_labels, y_labels
 
+def get_conn_datasource():
+    conn_mat = np.flip(data.connectivity_matrix, axis=0) # slipped so diagonal is top to bottom
+    label_names = data.labels.area
+    label_numbers = data.labels.index 
+    x_names, y_names = label_array(label_names)
+    x_numbers, y_numbers = label_array(label_numbers)
+    
+    source = ColumnDataSource(
+    data = {
+        "image" : [conn_mat],
+        "x_names": x_names,
+        "y_names": y_names,
+        "x_numbers": x_numbers,
+        "y_numbers": y_numbers,
+        })
+    return source
+
 def plot_conn_mat():
-    #flipped_mat = np.identity(3) #np.corrcoef(np.random.randint(10, size=(5,10))) ##np.flip(data.connectivity_matrix, axis=0)
-    #label_names = ['a','b','c','d','e']  #list(data.labels.area)
-    #reverse_names = label_names[::-1]
-
-    location_source = ColumnDataSource(
-        data = {
-            "image" : [flipped_mat],
-            "label_names": [],
-            "reverse_names": []
-            "label_numbers": list(data.labels.index)
-            "reverse_numbers": label_numbers[::-1]
-            })
-        [data["label_names"], data["reverse_names"] = name_array(labels)
-
-    # maybe use later the numbers
-    # label_numbers = list(data.labels.index) 
-    # reverse_numbers = label_numbers[::-1]
-
-    conn_mat_fig.image("image", x = 0, y = 0, dw = 7, dh = 7, source = location_source)  
-
-    # add labels to axes and place them in the middle of each box
-    ticks = [i+0.5 for i in range(len(label_names))]
-
-    x_label_dict = {i+0.5:label_names[i] for i in range(len(label_names))}
-    conn_mat_fig.xaxis.ticker = ticks
-    conn_mat_fig.xaxis.major_label_overrides = x_label_dict
-    conn_mat_fig.xaxis.major_label_orientation = np.pi/4
-
-    y_label_dict = {i+0.5:reverse_names[i] for i in range(len(label_names))}
-    conn_mat_fig.yaxis.ticker = ticks
-    conn_mat_fig.yaxis.major_label_overrides = y_label_dict
-
-    # currently not possible...
-    # hover = conn_mat_fig.select(dict(type=HoverTool))
-    # hover.tooltips =[("x label name", "@names_x"), ("y label name", "@names_y"), ("x label number", "@numbers_x"), ("y label number", "@numbers_y")]
-    # hover.mode = 'mouse'
-    conn_mat_fig.add_tools(HoverTool(tooltips =[("value:", "@image"), ("x", "@label_names"), ("y", "@reverse_names"), ("x_num", "@label_numbers"), ("y_num", "reverse_names"]))
+    source = get_conn_datasource()
+    conn_mat_fig.image("image", x = 0, y = 0, dw = 7, dh = 7, source = source)  
+    conn_mat_fig.add_tools(HoverTool(tooltips =[("value:", "@image"), ("x parcel name", "@x_names"), ("y parcel name", "@y_names"), 
+        ("x parcel number", "@x_numbers"), ("y parcel number", "@y_numbers")]))
+    conn_mat_fig.xaxis.visible = False
+    conn_mat_fig.yaxis.visible = False
 
 conn_button = Button(label="Compute Connectivity Matrix", width=int(total_width/4)*3+15, height=60)
 conn_button.on_click(plot_conn_mat)
@@ -108,17 +95,6 @@ nc_value = PreText(text=" ")
 ec_value = PreText(text=" ")
 mcc_value = PreText(text=" ")
 
-# Compute connectivity measure dropdown
-#cc = Button(label="Closness Centrality", width=int(total_width/8))
-#cc.on_click(ComputeConnectivityMeasureCC)
-#msp = Button(label="Mean Shortest Path", width=int(total_width/8))
-#msp.on_click(ComputeConnectivityMeasureMSP)
-#dd = Button(label="Degree Distribution", width=int(total_width/8))
-#dd.on_click(ComputeConnectivityMeasureDD)
-#nc = Button(label="Node Connectivity", width=int(total_width/8))
-#nc.on_click(ComputeConnectivityMeasureNC)
-#ec = Button(label="Edge Connectivity", width=int(total_width/8))
-#ec.on_click(ComputeConnectivityMeasureEC)
 for name, func in [
     ["Closness Centrality", ComputeConnectivityMeasureCC],
     ["Mean Shortest Path", ComputeConnectivityMeasureMSP],
@@ -168,7 +144,7 @@ def remove_brain(old_brain):
         ROI_fig.renderers.remove(glyph)
 
 def plot_ROI_fig():
-    img = data.seed_based()
+    img = data.seed_based
     if img is None:
         pass # add a warning
 
